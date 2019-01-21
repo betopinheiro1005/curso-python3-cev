e6c64fcb9eb50d191baa6180755f941bdbd46aaf
Roberto Pinheiro <betopinheiro1005@yahoo.com.br>
2019-01-21 19:26:38 -0200

    Aulas e desafios do curso de Python 3

---
+++ Desafios/desafio_002.py
@@ -0,0 +1,8 @@
+#Crie um script Python que leia dois números e tente
+#mostrar a soma entre eles.
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+s = n1 + n2
+
+print('A soma de ', n1 , '+' , n2 , 'é igual a: ' , s , '.')
